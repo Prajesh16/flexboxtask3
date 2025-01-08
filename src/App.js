702db45293a93124c4f7bdff762f178871aef014
Prajesh16 <prajesh.prajapati@spreadd.io>
2025-01-08 12:39:35 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="parent">
+        <div class="child">Center me.</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
